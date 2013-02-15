51e1a2a22263330cd83e397ce1bc952efc224299
Daniel Werthén <danielwerthen@gmail.com>
2013-02-15 15:23:44 +0100

    More stuff

---
+++ test/listener.js
@@ -0,0 +1,20 @@
+var should = require('should')
+	, util = require('util')
+	, _ = require('underscore')
+	, http = require('http')
+	, listener = require('../lib/listener')
+
+describe('Listener', function () {
+	it('simple IO', function (done) {
+		var srv = http.createServer(listener({
+			logger: function (num, str) { console.dir(str); }
+		}));
+		srv.listen(9001);
+
+		var req = http.request({ host: 'localhost', port: 9001, path: '/ping', method: 'POST' }, function (res) {
+		});
+		req.write(JSON.stringify({ time: Date.now() }));
+		req.end();
+		done();
+	});
+});
